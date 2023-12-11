18955776c58ad3944eb6ad4e6ec35e1ab4109623
Dima_Tatarenko <dmitriy.tatarenko@gmail.com>
2023-12-11 12:40:07 +0100

    erased domestic tag

--- src/models/flight.model.js
+++ src/models/flight.model.js
@@ -2,8 +2,8 @@ const selectById = (flightId) => {
     return db.query('select * from flights where flights.id = ?;', [flightId])
 }
 
-const insertFlight = ({ origin_id, destination_id, departure, arrival, duration, domestic, price, available_seats, available_luggage, terminal, gate, img }) => {
-    return db.query('insert into flights (origin_id, destination_id, departure,arrival,duration,domestic,price,available_seats,available_luggage,terminal,gate,img) values (?,?,?,?,?,?,?,?,?,?,?,?)', [origin_id, destination_id, departure, arrival, duration, domestic, price, available_seats, available_luggage, terminal, gate, img])
+const insertFlight = ({ origin_id, destination_id, departure, arrival, duration, price, available_seats, available_luggage, terminal, gate, img }) => {
+    return db.query('insert into flights (origin_id, destination_id, departure,arrival,duration,price,available_seats,available_luggage,terminal,gate,img) values (?,?,?,?,?,?,?,?,?,?,?,?)', [origin_id, destination_id, departure, arrival, duration, price, available_seats, available_luggage, terminal, gate, img])
 }
 
 
